03b1a958520e8c0d0f275161eb7d24ee2c796ec8
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-04-25 20:47:31 +0700

    feat: save file result to file

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const fs = require('fs');
+
 const cube = require('./app/utils/cube');
 const cuboid = require('./app/utils/cuboid');
 const triangularPrism = require('./app/utils/triangularPrism');
 
-console.log('Volume of cube:', cube.cubeVolume(5));
+const cubeVolume = cube.cubeVolume(5);
+const cuboidVolume = cuboid.cuboidVolume(3, 4, 5);
+const triangularPrismVolume = triangularPrism.triangularPrismVolume(4, 3, 6);
 
-console.log('Volume of cuboid:', cuboid.cuboidVolume(3, 4, 5));
+const resultText = `Volume of cube: ${cubeVolume}
+Volume of cuboid: ${cuboidVolume}
+Volume of triangular prism: ${triangularPrismVolume}`;
 
-console.log('Volume of triangular prism:', triangularPrism.triangularPrismVolume(4, 3, 6));
+fs.writeFileSync('result.txt', resultText, 'utf8');
+
+const content = fs.readFileSync('result.txt', 'utf8');
+
+console.log('Content of result.txt:');
+console.log(content);
